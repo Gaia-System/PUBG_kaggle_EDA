12c40e2dc15e06e2117ad6010efb1b7fb3de0a8a
Gaia-System <melon000999@gmail.com>
2022-06-09 15:21:53 +0900

    2덩어리였던 코드를 1개로 묶음

--- featureImportance.py
+++ featureImportance.py
@@ -1,8 +1,8 @@
 def featureImportanceForest(X_data, y_data, n_estimators, n_jobs,  min_samples_leaf = 1, graph_title = 'The Graph', random_state = 42, only_graph = True):
     from sklearn.ensemble import RandomForestRegressor
 
-    if only_graph == False:
-        try:
+    try:
+        if only_graph == False:
             rfr = RandomForestRegressor(n_estimators = n_estimators, min_samples_leaf = min_samples_leaf, n_jobs = n_jobs, random_state = random_state)
             rfr.fit(X_data, y_data)
             importances = rfr.feature_importances_
@@ -16,17 +16,7 @@ def featureImportanceForest(X_data, y_data, n_estimators, n_jobs,  min_samples_l
             plt.show()
             return np.array([importances, std, indices])
 
-        except ValueError:
-            print('Please check column(s) in data frame whether that includes object or NaN.')
-        except NameError:
-            print(
-                'Please import modules \n'
-                'import numpy as np \n'
-                'import matplotlib.pyplot as plt'
-            )
-
-    else:
-        try:
+        else:
             rfr = RandomForestRegressor(n_estimators = n_estimators, min_samples_leaf = min_samples_leaf, n_jobs = n_jobs, random_state = random_state)
             rfr.fit(X_data, y_data)
             importances = rfr.feature_importances_
@@ -38,13 +28,15 @@ def featureImportanceForest(X_data, y_data, n_estimators, n_jobs,  min_samples_l
             plt.bar(range(X_data.shape[1]), importances[indices], color = 'r', yerr = std[indices], align = 'center')
             plt.xticks(range(X_data.shape[1]), X_data.columns[indices], rotation = 90, fontsize = 13)
             plt.show()
-        except ValueError:
-            print('Please check column(s) in data frame whether that includes object or NaN.')
-        except NameError:
-            print(
-                'Please import modules \n'
-                'import numpy as np \n'
-                'import matplotlib.pyplot as plt'
+
+    except ValueError:
+        print('Please check column(s) in data frame whether that includes object or NaN.')
+    except NameError:
+        print(
+            'Please import modules \n'
+            'import numpy as np \n'
+            'import matplotlib.pyplot as plt'
             )
+
         # except pd.core.Indexing.IndexingError:    # I think the IndexingError isn't default error type in pure python.
         #     print('Please check range of y_data, if y_data is Series you never write 2nd colon that means columns')
