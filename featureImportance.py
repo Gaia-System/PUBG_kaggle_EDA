ed6d0ac2ac12df0a724762e8051a1c07b3fdccf3
Gaia-System <melon000999@gmail.com>
2022-06-09 11:08:10 +0900

    add some default values in method

--- featureImportance.py
+++ featureImportance.py
@@ -1,4 +1,4 @@
-def featureImportanceForest(X_data, y_data, n_estimators, min_samples_leaf, n_jobs, random_state = 42, only_graph = True):
+def featureImportanceForest(X_data, y_data, n_estimators, n_jobs,  min_samples_leaf = 1, graph_title = 'The Graph', random_state = 42, only_graph = True):
     import matplotlib.pyplot as plt
     import numpy as np
     from sklearn.ensemble import RandomForestRegressor
@@ -12,6 +12,7 @@ def featureImportanceForest(X_data, y_data, n_estimators, min_samples_leaf, n_jo
             indices = np.argsort(importances)[::-1]
 
             plt.figure(figsize = (15,15))
+            plt.title(graph_title, fontdict = {'fontsize':15})
             plt.bar(range(X_data.shape[1]), importances[indices], color = 'r', yerr = std[indices], align = 'center')
             plt.xticks(range(X_data.shape[1]), X_data.columns[indices], rotation = 90, fontsize = 13)
             plt.show()
@@ -29,10 +30,11 @@ def featureImportanceForest(X_data, y_data, n_estimators, min_samples_leaf, n_jo
             indices = np.argsort(importances)[::-1]
 
             plt.figure(figsize = (15,15))
+            plt.title(graph_title, fontdict = {'fontsize':15})
             plt.bar(range(X_data.shape[1]), importances[indices], color = 'r', yerr = std[indices], align = 'center')
             plt.xticks(range(X_data.shape[1]), X_data.columns[indices], rotation = 90, fontsize = 13)
             plt.show()
         except ValueError:
             print('Please check column(s) in data frame whether that includes object or NaN.')
-        # except pd.core.Indexing.IndexingError:
+        # except pd.core.Indexing.IndexingError:    # I think the IndexingError isn't default error type in python.
         #     print('Please check range of y_data, if y_data is Series you never write 2nd colon that means columns')
