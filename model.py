e839d4a1e098789a8be892d4d7455a4b4cd0a35b
Gaia-System <melon000999@gmail.com>
2022-06-09 16:08:38 +0900

    add the model.py description

--- model.py
+++ model.py
@@ -1,3 +1,9 @@
+'''
+X_train, y_train, X_test, y_test와 모델에 넣을 feature를 입력하면
+그에 대한 Linear regression 모델이 생성됨과 동시에
+MSE, MAE, R2 지표의 값이 소수점 4번째까지 반환됩니다.
+'''
+
 def linearEvaluate(X_train, y_train, X_test, y_test, feature_list):
     from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
     from sklearn.linear_model import LinearRegression
